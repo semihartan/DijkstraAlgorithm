46514bf157670c61d5ca2e5173b60aeabb71fb00
semihartan <s-785412369@hotmail.com>
2023-07-28 15:47:37 +0300

    Add third example and update example numbering to start from zero.

--- DijkstraAlgorithm/Program.cs
+++ DijkstraAlgorithm/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        private static GraphPath Example1()
+        private static GraphPath Example0()
         {
             Graph graph = new Graph();
             GraphNode nodeA = new GraphNode("A");
@@ -29,7 +29,7 @@
              
             return graph.FindShortestPathFromTo(nodeA, nodeC);
         }
-        private static GraphPath Example2()
+        private static GraphPath Example1()
         {
             Graph graph = new Graph();
             GraphNode node1 = new GraphNode("1");
@@ -52,10 +52,45 @@
             
             return graph.FindShortestPathFromTo(node1, node2);
         }
+        private static GraphPath Example2()
+        {
+            Graph graph = new Graph();
+            GraphNode node1 = new GraphNode("1");
+            GraphNode node2 = new GraphNode("2");
+            GraphNode node3 = new GraphNode("3");
+            GraphNode node4 = new GraphNode("4");
+            GraphNode node5 = new GraphNode("5");
+            GraphNode node6 = new GraphNode("6");
+            graph.AddNode(node1);
+            graph.AddNode(node2);
+            graph.AddNode(node3);
+            graph.AddNode(node4);
+            graph.AddNode(node5);
+            graph.AddNode(node6);
+            graph.AddEdge(new GraphEdge(node1, node2, 3));
+            graph.AddEdge(new GraphEdge(node1, node3, 5));
+            graph.AddEdge(new GraphEdge(node1, node4, 9));
+
+            graph.AddEdge(new GraphEdge(node2, node3, 3));
+            graph.AddEdge(new GraphEdge(node2, node4, 4));
+            graph.AddEdge(new GraphEdge(node2, node5, 7));
+             
+            graph.AddEdge(new GraphEdge(node3, node4, 2));
+            graph.AddEdge(new GraphEdge(node3, node5, 6));
+            graph.AddEdge(new GraphEdge(node3, node6, 8));
+
+            graph.AddEdge(new GraphEdge(node4, node5, 2));
+            graph.AddEdge(new GraphEdge(node4, node6, 2));
+
+            graph.AddEdge(new GraphEdge(node5, node6, 5));
+
+            return graph.FindShortestPathFromTo(node1, node6);
+        }
         static void Main(string[] args)
         {
             List<GraphPath> shortestPathsFromExamples = new List<GraphPath>
             {
+                Example0(),
                 Example1(),
                 Example2()
             };
